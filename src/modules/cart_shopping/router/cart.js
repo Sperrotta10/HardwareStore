fc9ec85a4d6a67a5e7730566a8c0ad485282d283
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-08 16:00:09 -0400

    create (router --> module cart): se creo el router del modulo

--- src/modules/cart_shopping/router/cart.js
+++ src/modules/cart_shopping/router/cart.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { ControllerCart } from "../controller/cart.js";
 
 export const cartRouter = Router();
 
-
+cartRouter.get('/', ControllerCart.getAllCart); // ✅
+cartRouter.get('/:cart_id', ControllerCart.getIdCart); // ✅
+cartRouter.post('/cart', ControllerCart.createCart); // ✅
+cartRouter.post('/itemCart', ControllerCart.createItemCart); // ✅
+cartRouter.patch('/:item_id', ControllerCart.updateItemCart); // ✅
+cartRouter.delete('/:item_id', ControllerCart.deleteItemCart); // ✅
 
