74f6bec04cb72302f09c06f085a4a504e3ad8867
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-07 22:23:10 -0400

    refactor (module --> cart): se modifico la estructura del archivo a una funcion

--- src/modules/cart_shopping/schemas/cart.js
+++ src/modules/cart_shopping/schemas/cart.js
@@ -1,41 +1,33 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../../../config/dataBase";
-import { User } from "../../register/schemas/user.js";
-import { itemCart } from "./itemCart.js";
 
-export const Cart = sequelize.define('Cart', {
-    cart_id : {
-        type : DataTypes.INTEGER,
-        autoIncrement : true,
-        primaryKey : true,
-    },
-    user_id : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        references : {
-            model : User,
-            key : 'user_id'
+export function defineCart(sequelize, DataTypes) {
+ 
+    const Cart = sequelize.define('Cart', {
+        cart_id : {
+            type : DataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey : true,
+        },
+        user_id : {
+            type : DataTypes.INTEGER,
+            allowNull : false,
+            references : {
+                model : 'User',
+                key : 'user_id'
+            }
+        },
+        created_at : {
+            type : DataTypes.DATEONLY,
+            allowNull : false,
+        },
+        updated_at : {
+            type : DataTypes.DATEONLY,
+            allowNull : false,
         }
-    },
-    created_at : {
-        type : DataTypes.DATEONLY,
-        allowNull : false,
-    },
-    updated_at : {
-        type : DataTypes.DATEONLY,
-        allowNull : false,
-    }
-}, {
-    tableName : 'Cart',
-    timestamps : false,
-})
+    }, {
+        tableName : 'Cart',
+        timestamps : false,
+    })
 
-Cart.belongsTo(User, {
-    foreignKey : 'user_id',
-    as : 'user',
-})
-
-Cart.hasMany(itemCart, {
-    foreignKey : 'cart_id',
-    as : 'itemCart'
-})
+    return Cart;
+    
+}
