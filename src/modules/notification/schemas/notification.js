5edb8eb7c2fe51b042740836aa94558b95cff27a
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-07 22:25:19 -0400

    refactor (module --> notification): se modifico la estructura del archivo a una funcion

--- src/modules/notification/schemas/notification.js
+++ src/modules/notification/schemas/notification.js
@@ -1,39 +1,36 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../../../config/dataBase";
-import { Product } from "../../products/schemas/product.js";
 
-export const Notification = sequelize.define('Notification', {
-    notification_id : {
-        type : DataTypes.INTEGER,
-        autoIncrement : true,
-        primaryKey : true,
-    },
-    product_id : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        references : {
-            model : Product,
-            key : 'product_id',
-        }
-    },
-    message : {
-        type : DataTypes.TEXT,
-        allowNull : false,
-    },
-    notification_status : {
-        type : DataTypes.BOOLEAN,
-        allowNull : false,
-    },
-    created_at : {
-        type : DataTypes.DATEONLY,
-        allowNull : false,
-    },
-}, {
-    tableName : 'Notification',
-    timestamps : false,
-})
+export function defineNotification(sequelize, DataTypes) {
+ 
+    const Notification = sequelize.define('Notification', {
+        notification_id : {
+            type : DataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey : true,
+        },
+        product_id : {
+            type : DataTypes.INTEGER,
+            allowNull : false,
+            references : {
+                model : 'Product',
+                key : 'product_id',
+            }
+        },
+        message : {
+            type : DataTypes.TEXT,
+            allowNull : false,
+        },
+        notification_status : {
+            type : DataTypes.BOOLEAN,
+            allowNull : false,
+        },
+        created_at : {
+            type : DataTypes.DATEONLY,
+            allowNull : false,
+        },
+    }, {
+        tableName : 'Notification',
+        timestamps : false,
+    })
 
-Notification.belongsTo(Product, {
-    foreignKey : 'product_id',
-    as : 'product',
-})
+    return Notification;
+}
