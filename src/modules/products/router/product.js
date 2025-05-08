96dceb58a9299dacd7cf3201060404cd6c4ede44
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-07 22:26:30 -0400

    add (router --> module products): se implemento las rutas (get, post, patch, delete)

--- src/modules/products/router/product.js
+++ src/modules/products/router/product.js
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { ControllerProduct } from "../controller/product.js";
 
 export const productsRouter = Router();
+
+productsRouter.get('/', ControllerProduct.getAll); // ✅
+productsRouter.get('/:product_id', ControllerProduct.getId); // ✅
+productsRouter.post('/', ControllerProduct.create); // ✅
+productsRouter.patch('/:product_id', ControllerProduct.updateId); // ✅
+//productsRouter.put('/products-name', ControllerProduct.updateName);
+productsRouter.delete('/:product_id', ControllerProduct.delete); // ✅
