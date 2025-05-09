ce3fac16557afb43e235809837eb70f04097ee39
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-09 18:00:18 -0400

    feat (router module --> register): se agrego las solicitudes correspondientes para este modulo

--- src/modules/register/router/register.js
+++ src/modules/register/router/register.js
@@ -1,3 +1,10 @@
 import { Router } from "express";
+import { ControllerRegister } from "../controller/register.js";
 
 export const registerRouter = Router();
+
+registerRouter.get('/', ControllerRegister.getAll); // ✅
+registerRouter.get('/:user_id', ControllerRegister.getId); // ✅
+registerRouter.post('/', ControllerRegister.create); // ✅
+registerRouter.patch('/:user_id', ControllerRegister.update); // ✅
+registerRouter.delete('/:user_id', ControllerRegister.delete); // ✅
