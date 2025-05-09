64e3a230f4996ff4cb75bd279498fcdb298482e8
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-09 14:48:12 -0400

    feat (router module --> notification): se implemento las peticiones correspondientes para este modulo

--- src/modules/notification/router/notification.js
+++ src/modules/notification/router/notification.js
@@ -1,3 +1,10 @@
 import { Router } from "express";
+import { ControllerNotification } from "../controller/notification.js";
 
 export const notificationRouter = Router();
+
+notificationRouter.get('/active', ControllerNotification.getAllActive);
+notificationRouter.get('/disable', ControllerNotification.getAllDisable);
+notificationRouter.post('/', ControllerNotification.create);
+notificationRouter.patch('/:notification_id', ControllerNotification.update);
+notificationRouter.delete('/:notification_id', ControllerNotification.delete);
