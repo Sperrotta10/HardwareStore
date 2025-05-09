9ded60540b461be769d0b4a3fb0eff4d8637ff8e
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-09 17:59:27 -0400

    create (validation module --> register): se agrego las validaciones de este modulo

---
+++ src/modules/register/validation/create.js
@@ -0,0 +1,15 @@
+import zod from "zod";
+
+const validation = zod.object({
+    user_name : zod.string().min(3).max(30, {message : "El usuario tiene que ser un string entre 3 y 30 caracteres"}),
+    email : zod.string().email({message : "El email tiene que ser un string"}),
+    password : zod.string().min(6, {message : "El password tiene que ser un string y como minimo 6 caracteres"}),
+    role_id : zod.number().nonnegative({message : "El role tiene que ser un id"}),
+    user_status : zod.boolean({message : "El status tiene que ser un booleano"}).default(true),
+    created_at : zod.date({message : "Tiene que ser un dato de tipo date"}).default(() => new Date()),
+    updated_at : zod.date({message : "Tiene que ser un dato de tipo date"}).default(() => new Date())
+});
+
+export function validationCreate(data) {
+    return validation.safeParse(data);
+}
