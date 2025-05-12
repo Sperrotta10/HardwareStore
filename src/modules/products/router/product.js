e6e10e57e31df0cb1607addf55a2574d7016b447
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-11 21:59:01 -0400

    feat (module products: se agrego los middlewares correspondientes

--- src/modules/products/router/product.js
+++ src/modules/products/router/product.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { ControllerProduct } from "../controller/product.js";
+import { authMiddleware } from "../../../middlewares/authentification.js"
+import { authorizeRole } from "../../../middlewares/authorizeRole.js"
 
 export const productsRouter = Router();
 
 productsRouter.get('/', ControllerProduct.getAll); // ✅
 productsRouter.get('/:product_id', ControllerProduct.getId); // ✅
-productsRouter.post('/', ControllerProduct.create); // ✅
-productsRouter.patch('/:product_id', ControllerProduct.updateId); // ✅
+productsRouter.post('/', authMiddleware, authorizeRole('admin'), ControllerProduct.create); // ✅
+productsRouter.patch('/:product_id', authMiddleware, authorizeRole('admin'), ControllerProduct.updateId); // ✅
 //productsRouter.put('/products-name', ControllerProduct.updateName);
-productsRouter.delete('/:product_id', ControllerProduct.delete); // ✅
+productsRouter.delete('/:product_id', authMiddleware, authorizeRole('admin'), ControllerProduct.delete); // ✅
