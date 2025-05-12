edd9edb45d7dc8a379cb15bdc0065f5a996a61f1
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-11 21:59:23 -0400

    feat (module register): se agrego los middlewares correspondientes

--- src/modules/register/router/register.js
+++ src/modules/register/router/register.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { ControllerRegister } from "../controller/register.js";
+import { authMiddleware } from "../../../middlewares/authentification.js";
+import { authorizeRole } from "../../../middlewares/authorizeRole.js";
+import { verifyUserOwner } from "../../../middlewares/register/registerAuth.js";
 
 export const registerRouter = Router();
 
-registerRouter.get('/', ControllerRegister.getAll); // ✅
-registerRouter.get('/:user_id', ControllerRegister.getId); // ✅
+registerRouter.get('/', authMiddleware, authorizeRole('admin'), ControllerRegister.getAll); // ✅
+registerRouter.get('/:user_id', authMiddleware, authorizeRole('admin'), ControllerRegister.getId); // ✅
 registerRouter.post('/', ControllerRegister.create); // ✅
-registerRouter.patch('/:user_id', ControllerRegister.update); // ✅
-registerRouter.delete('/:user_id', ControllerRegister.delete); // ✅
+registerRouter.patch('/:user_id', authMiddleware, authorizeRole('admin', 'user'), verifyUserOwner, ControllerRegister.update); // ✅
+registerRouter.delete('/:user_id', authMiddleware, authorizeRole('admin'), ControllerRegister.delete); // ✅
