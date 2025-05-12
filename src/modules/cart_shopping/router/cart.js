d3f417943b2f1955aed3e7ce2da2ce39376ced66
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-11 21:58:11 -0400

    feat (module cart-shopping): se agrego los middlewares correspondientes

--- src/modules/cart_shopping/router/cart.js
+++ src/modules/cart_shopping/router/cart.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { ControllerCart } from "../controller/cart.js";
+import { authMiddleware } from "../../../middlewares/authentification.js";
+import { authorizeRole } from "../../../middlewares/authorizeRole.js";
+import { verifyCartOwner } from "../../../middlewares/cart_shopping/cartAuth.js";
+import { verifyItemOwner } from "../../../middlewares/cart_shopping/itemCartAuth.js";
 
 export const cartRouter = Router();
 
-cartRouter.get('/', ControllerCart.getAllCart); // ✅
-cartRouter.get('/:cart_id', ControllerCart.getIdCart); // ✅
-cartRouter.post('/cart', ControllerCart.createCart); // ✅
-cartRouter.post('/itemCart', ControllerCart.createItemCart); // ✅
-cartRouter.patch('/:item_id', ControllerCart.updateItemCart); // ✅
-cartRouter.delete('/:item_id', ControllerCart.deleteItemCart); // ✅
+cartRouter.get('/', authMiddleware, authorizeRole('admin'), ControllerCart.getAllCart); // ✅
+cartRouter.get('/:cart_id', authMiddleware, authorizeRole('user', 'admin'), verifyCartOwner, ControllerCart.getIdCart); // ✅
+cartRouter.post('/cart', authMiddleware, authorizeRole('user'), ControllerCart.createCart); // ✅
+cartRouter.post('/itemCart', authMiddleware, authorizeRole('user'), ControllerCart.createItemCart); // ✅
+cartRouter.patch('/:item_id', authMiddleware, authorizeRole('user'), verifyItemOwner, ControllerCart.updateItemCart); // ✅
+cartRouter.delete('/:item_id', authMiddleware, authorizeRole('user'), verifyItemOwner, ControllerCart.deleteItemCart); // ✅
 
