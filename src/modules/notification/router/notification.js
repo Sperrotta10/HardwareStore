843ef1a0d426ecc4c9411c867bab4c2d8dd66686
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-11 21:58:41 -0400

    feat (module notification): se agrego los middlewares correspondientes

--- src/modules/notification/router/notification.js
+++ src/modules/notification/router/notification.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { ControllerNotification } from "../controller/notification.js";
+import { authMiddleware } from "../../../middlewares/authentification.js";
+import { authorizeRole } from "../../../middlewares/authorizeRole.js"
 
 export const notificationRouter = Router();
 
-notificationRouter.get('/active', ControllerNotification.getAllActive);
-notificationRouter.get('/disable', ControllerNotification.getAllDisable);
-notificationRouter.post('/', ControllerNotification.create);
-notificationRouter.patch('/:notification_id', ControllerNotification.update);
-notificationRouter.delete('/:notification_id', ControllerNotification.delete);
+notificationRouter.get('/active', authMiddleware, authorizeRole('admin'), ControllerNotification.getAllActive);
+notificationRouter.get('/disable', authMiddleware, authorizeRole('admin'), ControllerNotification.getAllDisable);
+notificationRouter.post('/', authMiddleware, ControllerNotification.create);
+notificationRouter.patch('/:notification_id', authMiddleware, authorizeRole('admin'), ControllerNotification.update);
+notificationRouter.delete('/:notification_id', authMiddleware, authorizeRole('admin'), ControllerNotification.delete);
