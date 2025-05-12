080803a84551f0bed140fb6dea84bbf604b0c400
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-05-11 21:58:27 -0400

    feat (module login): se agrego los middlewares correspondientes

--- src/modules/login/router/login.js
+++ src/modules/login/router/login.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { ControllerLogin } from "../controller/login.js";
 import { loginLimiter } from "../../../middlewares/loginLimit.js";
+import { authMiddleware } from "../../../middlewares/authentification.js";
+import { authorizeRole } from "../../../middlewares/authorizeRole.js";
 
 export const loginRouter = Router();
 
 loginRouter.post('/', loginLimiter, ControllerLogin.login); // ✅
-loginRouter.post('/refresh-token', ControllerLogin.refresh); // ✅
+loginRouter.post('/refresh-token', authMiddleware, authorizeRole('admin', 'user'), ControllerLogin.refresh); // ✅
